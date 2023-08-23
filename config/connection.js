459185f49415605c4434c447093772521349b5ae
rhonvyramos <rhonvyramos@gmail.com>
2023-08-23 11:18:09 -0400

    created connection to server

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+let sequelize;
+
+// syntax taken from Week 14 Mini Project 
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  } else {
+    sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASS,
+      {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+      }
+    );
+  }
+  
+  module.exports = sequelize;
