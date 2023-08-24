056628a5561a8d242ac2720eb0653b6bd932649e
rhonvyramos <rhonvyramos@gmail.com>
2023-08-24 12:30:08 -0400

    database seeding function created

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,2 +1,17 @@
 const sequelize = require("../config/connection");
 const { Post, Comment } = require("../models");
+
+const postSeedData =  require('./postData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const readers = await Post.bulkCreate(postSeedData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
